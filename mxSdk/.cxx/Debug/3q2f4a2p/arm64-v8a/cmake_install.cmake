3605bfd56f453e4f61cf888943ace043568b0de9
rbqren000 <renbaoqiang721@163.com>
2026-06-09 09:50:19 +0800

    更新

--- mxSdk/.cxx/Debug/3q2f4a2p/arm64-v8a/cmake_install.cmake
+++ mxSdk/.cxx/Debug/3q2f4a2p/arm64-v8a/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/rbq/AndroidStudioProjects/HalftoneAll/jzPrint/mxSdk/src/main/cpp
+# Install script for directory: /Users/rbq/AndroidStudioProjects/jzPrint/mxSdk/src/main/cpp
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/rbq/AndroidStudioProjects/HalftoneAll/jzPrint/mxSdk/.cxx/Debug/3q2f4a2p/arm64-v8a/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/rbq/AndroidStudioProjects/jzPrint/mxSdk/.cxx/Debug/3q2f4a2p/arm64-v8a/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
